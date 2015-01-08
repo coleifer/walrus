84e14782f353ef1d0dec20ed1da31cfb1da413a4
Charles Leifer <coleifer@gmail.com>
2015-01-08 00:49:51 -0600

    Add diary example.

---
+++ examples/diary.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from collections import OrderedDict
+import datetime
+import sys
+
+from walrus import *
+
+database = Database(host='localhost', port=6379, db=0)
+
+class Entry(Model):
+    database = database
+    namespace = 'diary'
+
+    content = TextField(fts=True)
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+
+def menu_loop():
+    choice = None
+    while choice != 'q':
+        for key, value in menu.items():
+            print('%s) %s' % (key, value.__doc__))
+        choice = raw_input('Action: ').lower().strip()
+        if choice in menu:
+            menu[choice]()
+
+def add_entry():
+    """Add entry"""
+    print('Enter your entry. Press ctrl+d when finished.')
+    data = sys.stdin.read().strip()
+    if data and raw_input('Save entry? [Yn] ') != 'n':
+        Entry.create(content=data)
+        print('Saved successfully.')
+
+def view_entries(search_query=None):
+    """View previous entries"""
+    if search_query:
+        expr = Entry.content.match(search_query)
+    else:
+        expr = None
+
+    query = Entry.query(expr, order_by=Entry.timestamp.desc())
+    for entry in query:
+        timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        print(timestamp)
+        print('=' * len(timestamp))
+        print(entry.content)
+        print('n) next entry')
+        print('q) return to main menu')
+        if raw_input('Choice? (Nq) ') == 'q':
+            break
+
+def search_entries():
+    """Search entries"""
+    view_entries(raw_input('Search query: '))
+
+menu = OrderedDict([
+    ('a', add_entry),
+    ('v', view_entries),
+    ('s', search_entries),
+])
+
+if __name__ == '__main__':
+    menu_loop()
