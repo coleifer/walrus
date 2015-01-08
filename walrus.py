1e06377b5fb6d684af76793cd2d06e5e38996629
Charles Leifer <coleifer@gmail.com>
2015-01-07 19:25:53 -0600

    version 0.1.1

--- walrus.py
+++ walrus.py
@@ -4,7 +4,7 @@ Lightweight Python utilities for working with Redis.
 
 __author__ = 'Charles Leifer'
 __license__ = 'MIT'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from copy import deepcopy
 from functools import wraps
