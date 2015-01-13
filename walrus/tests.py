3cdf6ca6e41d2b6ee2ccd89a2aef985b003055a3
Charles Leifer <coleifer@gmail.com>
2015-01-12 22:57:55 -0600

    Test the new decorators.

--- walrus/tests.py
+++ walrus/tests.py
@@ -44,6 +44,11 @@ cache = db.cache(name='test.cache')
 def now(seed=None):
     return datetime.datetime.now()
 
+class Clock(object):
+    @cache.cached_property()
+    def now(self):
+        return datetime.datetime.now()
+
 
 class WalrusTestCase(unittest.TestCase):
     def setUp(self):
@@ -461,6 +466,24 @@ class TestCache(WalrusTestCase):
         now.bust(1)
         self.assertNotEqual(now(1), n2)
         self.assertEqual(now(1), now(1))
+
+    def test_cached_property(self):
+        c = Clock()
+        n1 = c.now
+        n2 = c.now
+        self.assertEqual(n1, n2)
+
+    def test_cached_async(self):
+        @cache.cache_async()
+        def double_value(value):
+            return value * 2
+
+        res = double_value(3)
+        self.assertEqual(res(), 6)
+        self.assertEqual(res(), 6)
+
+        self.assertEqual(double_value(3)(), 6)
+        self.assertEqual(double_value(4)(), 8)
 
 
 class TestHash(WalrusTestCase):
