ac71e0210a94c01f20aedd91511ed72df573f5ea
Charles Leifer <coleifer@gmail.com>
2018-11-24 16:30:03 -0600

    0.7.1

--- walrus/__init__.py
+++ walrus/__init__.py
@@ -4,7 +4,7 @@ Lightweight Python utilities for working with Redis.
 
 __author__ = 'Charles Leifer'
 __license__ = 'MIT'
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 #               ___
 #            .-9 9 `\
